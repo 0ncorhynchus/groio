7e451fc1b985963eb12a1f2bc93b88166bc5e5a7
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-03-14 15:47:20 +0900

    Add display tests

--- src/types.rs
+++ src/types.rs
@@ -14,7 +14,7 @@ impl Vector3d {
 
 impl fmt::Display for Vector3d {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "({}, {}, {})", self.x, self.y, self.z)
+        write!(f, "{:>8.3}{:>8.3}{:>8.3}", self.x, self.y, self.z)
     }
 }
 
@@ -29,13 +29,13 @@ pub struct Atom {
 
 impl fmt::Display for Atom {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{} {} {} {} {} {}",
+        write!(f, "{:>5}{:<5}{:>5}{:>5}{}{}",
                self.res_number,
                self.res_name,
                self.atom_name,
                self.atom_number,
                self.position,
-               self.velocity,)
+               self.velocity)
     }
 }
 
@@ -43,5 +43,29 @@ pub struct Structure {
     pub title: String,
     pub atoms: Vec<Atom>,
     pub box_size: Vector3d,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_vector_to_string() {
+        let vector = Vector3d::new(1.0, 2.0, 3.0);
+        assert_eq!("   1.000   2.000   3.000", vector.to_string());
+    }
+
+    #[test]
+    fn test_atom_to_string() {
+        let atom = Atom {
+            res_number: 1,
+            res_name: "ALA".to_string(),
+            atom_name: "H".to_string(),
+            atom_number: 1,
+            position: Vector3d::new(1.0, 2.0, 3.0),
+            velocity: Vector3d::new(4.0, 5.0, 6.0)
+        };
+        assert_eq!("    1ALA      H    1   1.000   2.000   3.000   4.000   5.000   6.000", atom.to_string());
+    }
 }
 
