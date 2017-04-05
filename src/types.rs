3d448d361f77ef9876943d49bc7f66630e6fa24a
Suguru Kato <salmon.bioinformatics@gmail.com>
2017-04-05 12:40:53 +0900

    Add Structure::atoms(&self) -> &Vec<Atom>
    To hide Structure::atoms

--- src/types.rs
+++ src/types.rs
@@ -6,6 +6,21 @@ pub struct Vector3d {
     pub z: f32,
 }
 
+pub struct Atom {
+    pub res_number:  i32,
+    pub res_name:    String,
+    pub atom_name:   String,
+    pub atom_number: i32,
+    pub position:    Vector3d,
+    pub velocity:    Vector3d,
+}
+
+pub struct Structure {
+    pub title: String,
+    pub atoms: Vec<Atom>,
+    pub box_size: Vector3d,
+}
+
 impl Vector3d {
     pub fn new(x: f32, y: f32, z: f32) -> Self {
         Vector3d { x: x, y: y, z: z }
@@ -21,15 +36,6 @@ impl fmt::Display for Vector3d {
     }
 }
 
-pub struct Atom {
-    pub res_number:  i32,
-    pub res_name:    String,
-    pub atom_name:   String,
-    pub atom_number: i32,
-    pub position:    Vector3d,
-    pub velocity:    Vector3d,
-}
-
 impl fmt::Display for Atom {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "{:>5}{:<5}{:>5}{:>5}{:>8.3}{:>9.4}",
@@ -42,10 +48,10 @@ impl fmt::Display for Atom {
     }
 }
 
-pub struct Structure {
-    pub title: String,
-    pub atoms: Vec<Atom>,
-    pub box_size: Vector3d,
+impl Structure {
+    pub fn atoms(&self) -> &Vec<Atom> {
+        &self.atoms
+    }
 }
 
 impl fmt::Display for Structure {
@@ -90,6 +96,25 @@ mod tests {
             box_size: Vector3d::new(1.0, 2.0, 3.0)
         };
         assert_eq!("The Title\n    1.0000    2.0000    3.0000", structure.to_string());
+    }
+
+    #[test]
+    fn test_structure_atoms() {
+        let structure = Structure {
+            title: "The Title".to_string(),
+            atoms: vec![
+                Atom {
+                    res_number: 1,
+                    res_name: "ALA".to_string(),
+                    atom_name: "H".to_string(),
+                    atom_number: 1,
+                    position: Vector3d::new(1.0, 2.0, 3.0),
+                    velocity: Vector3d::new(4.0, 5.0, 6.0)
+                }
+            ],
+            box_size: Vector3d::new(11.0, 12.0, 13.0)
+        };
+        assert_eq!(1, structure.atoms().len());
     }
 }
 
